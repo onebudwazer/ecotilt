f5bf5b8f9118891c33563fee1e017f16ddb2e57c
Philippe <Philippe@Philippe-PC>
2013-04-17 14:28:38 +0200

    zone marseille

---
+++ project_mobile_appui/src/fr/ecotilt/appui/util/StaticZoneVar.java
@@ -0,0 +1,8 @@
+package fr.ecotilt.appui.util;
+
+import fr.ecotilt.appui.model.GeoCoord;
+
+public class StaticZoneVar {
+	public static final GeoCoord areaNorthWest = new GeoCoord(43.541662, 5.322231);
+	public static final GeoCoord areaSouthEst = new GeoCoord(43.195344, 5.900036);
+}
