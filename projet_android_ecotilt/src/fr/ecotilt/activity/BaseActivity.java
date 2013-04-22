b2c719f9b2059d8ac18d38b976f5126a649b2852
Philippe <Philippe@Philippe-PC>
2013-04-22 22:55:43 +0200

    add android project

---
+++ projet_android_ecotilt/src/fr/ecotilt/activity/BaseActivity.java
@@ -0,0 +1,34 @@
+package fr.ecotilt.activity;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import fr.ecotilt.rsc.R;
+
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public abstract class BaseActivity extends Activity {
+
+	protected ImageLoader imageLoader = ImageLoader.getInstance();
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.item_clear_memory_cache:
+				imageLoader.clearMemoryCache();
+				return true;
+			case R.id.item_clear_disc_cache:
+				imageLoader.clearDiscCache();
+				return true;
+			default:
+				return false;
+		}
+	}
+}
