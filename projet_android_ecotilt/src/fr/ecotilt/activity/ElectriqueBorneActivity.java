d44a81734410bf94f184129955142ae7c9ede4ec
Philippe <Philippe@Philippe-PC>
2013-04-27 22:40:09 +0200

    mobile appli

--- projet_android_ecotilt/src/fr/ecotilt/activity/ElectriqueBorneActivity.java
+++ projet_android_ecotilt/src/fr/ecotilt/activity/ElectriqueBorneActivity.java
@@ -1,41 +1,24 @@
 package fr.ecotilt.activity;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import fr.ecotilt.activity.asyntask.AsyncTaskPompe;
+import fr.ecotilt.activity.asyntask.OnTaskCompleted;
 import fr.ecotilt.appui.model.Pompe;
 import fr.ecotilt.rsc.R;
 
-public class ElectriqueBorneActivity extends Activity {
+public class ElectriqueBorneActivity extends Activity implements
+		OnTaskCompleted {
 
-	
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_electrique_borne);
-		new DownloadFilesTask().execute();
+	private ArrayList<String>		valuesUi	= new ArrayList<String>();
 
-	}
+	private ArrayAdapter<String>	adapter;
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,57 +27,50 @@ public class ElectriqueBorneActivity extends Activity {
 		return true;
 	}
 
-	private class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
-		
-		Pompe[] listPompe;;
-		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_electrique_borne);
+		AsyncTaskPompe atPompe = new AsyncTaskPompe(this);
+		atPompe.execute();
 
-		protected Long doInBackground(URL... urls) {
-			StringBuilder builder = new StringBuilder();
-			HttpClient client = new DefaultHttpClient();
-			HttpGet httpGet = new HttpGet(
-					"http://10.10.2.55:8081/project_mobile_webserviceA/wspompe");
-			try {
-				HttpResponse response = client.execute(httpGet);
-				
-				StatusLine statusLine = response.getStatusLine();
-				int statusCode = statusLine.getStatusCode();
-				if (statusCode == 200) {
-					HttpEntity entity = response.getEntity();
-					InputStream content = entity.getContent();
-					BufferedReader reader = new BufferedReader(
-							new InputStreamReader(content));
-					String line;
-					while ((line = reader.readLine()) != null) {
-						builder.append(line);
-					}
-					
-					String value = builder.toString();
-					ObjectMapper om = new ObjectMapper();
-					//this.listPompe = om.readValue(value, Pompe[].class);
-					
-//					for (int i = 0; i < listPompe.length; i++) {
-//						System.out.println(listPompe[i].toString());
-//					}
-					Log.e("Lecture du flux json", builder.toString());
-				} else {
-					Log.e("Lecture du flux json", builder.toString());
-				}
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-
-		protected void onProgressUpdate(Integer... progress) {
-			// setProgressPercent(progress[0]);
-		}
-
-		protected void onPostExecute(Long result) {
-			// showDialog("Downloaded " + result + " bytes");
-		}
+		final ListView listview = (ListView) findViewById(R.id.listview);
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, valuesUi);
+//		adapter = new StableArrayAdapter(ElectriqueBorneActivity.this,
+//				android.R.layout.simple_list_item_1, valuesUi);
+		listview.setAdapter(adapter);
 	}
 
+	@Override
+	public void onTaskCompleted(List<Pompe> listPompe) {
+		for (Pompe pompe : listPompe) {
+			valuesUi.add(pompe.getName());
+		}
+		adapter.notifyDataSetChanged();
+	}
+
+//	private class StableArrayAdapter extends ArrayAdapter<String> {
+//
+//		HashMap<String, Integer>	mIdMap	= new HashMap<String, Integer>();
+//
+//		public StableArrayAdapter(Context context, int textViewResourceId,
+//				List<String> objects) {
+//			super(context, textViewResourceId, objects);
+//			for (int i = 0; i < objects.size(); ++i) {
+//				mIdMap.put(objects.get(i), i);
+//			}
+//		}
+//
+//		@Override
+//		public long getItemId(int position) {
+//			String item = getItem(position);
+//			return mIdMap.get(item);
+//		}
+//
+//		@Override
+//		public boolean hasStableIds() {
+//			return true;
+//		}
+//	}
 }
