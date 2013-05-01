748f436b77ba52f56fbe13cb5fc712f68c26d9a2
Johan <Johan@Ripere>
2013-05-01 13:32:42 +0200

    Ajout du logo sur home page

--- projet_android_ecotilt/src/fr/ecotilt/activity/MainActivity.java
+++ projet_android_ecotilt/src/fr/ecotilt/activity/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getActionBar().setDisplayShowTitleEnabled(false);
 		setContentView(R.layout.activity_main);
 		GridView gridview = (GridView) findViewById(R.id.gridview);
 	    gridview.setAdapter(new ImageAdapter(this));
